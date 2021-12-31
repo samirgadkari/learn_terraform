72a24fe0fa7ca956038190f5b4848f86c89760dd
Samir Gadkari <samir.gadkari@gmail.com>
2021-12-30 17:09:54 -0800

    Created server_port variable and used it within file

--- Ch2/main.tf
+++ Ch2/main.tf
@@ -39,6 +39,12 @@ provider "aws" {
 #  }
 #}
 
+variable "server_port" {
+  description = "The port used by the server for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -48,9 +54,9 @@ resource "aws_security_group" "instance" {
   # limited to the IP addr range 0.0.0.0/0 (all possible IP addresses given in a CIDR block format).
   # 10.0.0.0/24 is all IP addresses between 10.0.0.0 and 10.0.0.255 (high 24 bits fixed).
   ingress {
-    from_port = 8080  # We use 8080 instead of port 80 for HTTP because any port less than 1024
-    to_port   = 8080  # requires root user privileges. If an attacker compromisses your server,
-		      # they will get root access too.
+    from_port = var.server_port  # We use 8080 instead of port 80 for HTTP because any port less than 1024
+    to_port   = var.server_port  # requires root user privileges. If an attacker compromisses your server,
+				 # they will get root access too.
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -68,18 +74,33 @@ resource "aws_instance" "example" {
                                                             # Use Graphviz to display it.
   
   # user_data points to the shell script to run after instance boots up.
-  user_data = <<-EOF
+  # nohup command & causes command to run forever.
+  # busybox provides a suite of unix utilities,
+  # including httpd, the web server.
+  # The web server is listening to port 8080.
+    user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html  # index.html file has "Hello, World" in it now.
-              nohup busybox httpd -f -p 8080 &  # nohup command & causes command to run forever.
-                                                # busybox provides a suite of unix utilities,
-                                                # including httpd, the web server.
-                                                # The web server is listening to port 8080.
+              echo "Hello, World" > index.html  
+              nohup busybox httpd -f -p "${var.server_port}" &  
               EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+output "public_ip" {               # All output variables are printed out at the end of the
+				   # terraform apply command output.
+				   # You can also issue the "terraform output" command to see
+				   # these variables, or "terraform output public_ip" to see
+				   # a specific variable.
+  value       = aws_instance.example.public_ip
+  description = "Public IP address of the web server"
+}
+
+output "server_port" {
+  value       = var.server_port
+  description = "Server port number to access web server"
 }
 
 # We deployed these resources in the default VPC, and the default subnets of that VPC.
@@ -92,3 +113,19 @@ resource "aws_instance" "example" {
 # can then be accessible from within the same VPC. The only servers that should be accessible
 # from public IP addresses are the load balancers. These you should run in a public subnet
 # and lock down as much as possible.
+
+# Creating variables and referencing them:
+# ex:
+# variable "number_example" {
+#   description = "......"
+#   type        = number  # Possible types: string, number, bool, list, map, set, object, tuple, any.
+#			  # You can also have list(number) type (ex. [1, 2, 3]), or
+#			  # map(string) type (ex. {key1 = "a", key2 = "b"}), or
+#			  # object({name = string, age = number{) type (ex. {name = "a", age = 2}).
+#   default     = 42      # You can pass in the variable on the command line using -var.
+#			  # Pass via a file using -var-file on the command line.
+#			  # Pass via an environment variable using TF_VAR_<variable_name>.
+#			  # If no value passed in, the default given here is used.
+# }
+# The types you can create are: string, number, bool, list, map, set, object, tuple, any.
+# If not specified, type is assumed to be any.
